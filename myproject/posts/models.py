cc77c6e1775b55ba83faebec411cc80ed5b6df19
ayubshaik <ayubshaik642@gmail.com>
2024-08-01 21:56:26 +0530

    Django ORM

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+#adding a method to our post class, 
+#a method is like a function except its part of a class
+    
+    def __str__(self):
+        return self.title
